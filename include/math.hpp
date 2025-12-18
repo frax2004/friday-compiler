5e571d80acdbc607e14db00e410766c805f0569c
Francesco Pollarà <PolarFrancy@gmail.com>
2025-12-18 18:54:47 +0100

    Test fridayc::math::cos

---
+++ include/math.hpp
@@ -0,0 +1,8 @@
+#ifndef FRIDAYC_MATH_HPP
+#define FRIDAYC_MATH_HPP
+
+namespace fridayc::math {
+  using std::cos;
+}
+
+#endif
---
+++ tests/test_test.cpp
@@ -0,0 +1,9 @@
+#include "math.hpp"
+
+using namespace fridayc;
+
+auto main() -> int {
+  constexpr float x = 0;
+
+  assert(math::cos(x) == 1);
+}
