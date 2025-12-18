0352da729794331d537bdcc8bf7b923ec08a8d02
Francesco Pollarà <PolarFrancy@gmail.com>
2025-12-18 19:02:23 +0100

    Added support for builtin math functions

--- include/math.hpp
+++ include/math.hpp
@@ -3,6 +3,24 @@
 
 namespace fridayc::math {
   using std::cos;
+  using std::sin;
+  using std::tan;
+  using std::acos;
+  using std::asin;
+  using std::atan;
+  using std::atan2;
+  using std::pow;
+  using std::sqrt;
+  using std::cbrt;
+  using std::exp;
+  using std::log;
+  using std::log2;
+  using std::log10;
+  using std::lerp;
+  using std::clamp;
+  using std::max;
+  using std::min;
+
 }
 
 #endif
--- tests/test_test.cpp
+++
@@ -1,9 +0,0 @@
-#include "math.hpp"
-
-using namespace fridayc;
-
-auto main() -> int {
-  constexpr float x = 0;
-
-  assert(math::cos(x) == 1);
-}
